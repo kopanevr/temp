015ef5fcc36c8826ad876e83fec62c0ad19c201b
kopanevr <kopanevrsln@gmail.com>
2026-09-25 12:36:29 +0300

    tempCommit

--- src/Application/Subsystems/FrameCapture/include/FrameBuffer.hpp
+++ src/Application/Subsystems/FrameCapture/include/FrameBuffer.hpp
@@ -21,23 +21,29 @@ class FrameBuffer final {
 public:
   /// @brief Конструктор.
   FrameBuffer() {
-
   }
 
+  /// @brief Деструктор.
+  ~FrameBuffer() = default;
+
+  FrameBuffer(const FrameBuffer&) = delete;
+  FrameBuffer& operator=(const FrameBuffer&) = delete;
 
   /// @brief
   /// @param frame
-  void push(const cv::Mat &) {
-    std::lock_guard<std::mutex> lock(mutex_);
+  void push(const cv::Mat &frame) {
+    std::unique_lock<std::mutex> lock(mutex_);
 
     SET_FLAG(0, isFramePushed);
     cv_.notify_one();
   }
 
   /// @brief
-  void pop() {
+  void pop(cv::Mat &frame) {
     std::unique_lock<std::mutex> lock(mutex_);
     cv_.wait(lock, [this]{ return GET_FLAG_STATE(0, isFramePushed); });
+
+    RESET_FLAG(0, isFramePushed);
   }
 
 private:
@@ -45,9 +51,15 @@ private:
   BIT_FIELD(0, 1, FLAG(isFramePushed));
 
   /// @brief
+  static inline FrameBuffer *instance_;
+
+  /// @brief
   std::mutex mutex_;
 
   /// @brief
   std::condition_variable cv_;
+
+  /// @brief
+  cv::Mat frame_;
 };
 } // namespace frameCapture
